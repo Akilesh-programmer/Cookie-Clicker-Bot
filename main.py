0a4c976dc6e8f19b8da3222a36259d1368ebb0df
Akilesh-programmer <akileshakileshs1234@gmail.com>
2022-01-12 16:50:40 +0530

    P

--- main.py
+++ main.py
@@ -3,13 +3,30 @@ from selenium import webdriver
 chrome_driver_path = "C:\Development\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-cookie = driver.find_element_by_xpath('//*[@id="bigCookie"]')
+cookie = driver.find_element_by_xpath('//*[@id="cookie"]')
 
 game_is_on = True
 
+store = driver.find_elements_by_css_selector("#store div")
+new_store = store[::-1]
+
+def game():
+
+
 while game_is_on:
-    for n in range(200):
+    for n in range(150):
         cookie.click()
+
+    print("Hello World")
+    
+    for thing in new_store:
+        print(thing.get_attribute("id"))
+        try:
+            thing.click()
+        except:
+            pass
+        else:
+            game()
     
